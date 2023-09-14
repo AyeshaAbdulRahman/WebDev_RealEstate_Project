7ae99b642eb84793c365208624902b949145c873
Ayesha A.Rahman <132191114+AyeshaAbdulRahman@users.noreply.github.com>
2023-09-15 03:04:22 +0500

    Add files via upload

---
+++ about.php
@@ -0,0 +1,129 @@
+<?php 
+ini_set('session.cache_limiter','public');
+session_cache_limiter(false);
+session_start();
+include("config.php");								
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Meta Tags -->
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="Real Estate PHP">
+<meta name="keywords" content="">
+<meta name="author" content="Unicoder">
+<link rel="shortcut icon" href="images/favicon.ico">
+
+<!-- Fonts -->
+<link href="https://fonts.googleapis.com/css?family=Muli:400,400i,500,600,700&amp;display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Comfortaa:400,700" rel="stylesheet">
+
+<!-- Css Link -->
+<!-- FOR MORE PROJECTS visit: codeastro.com -->
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap-slider.css">
+<link rel="stylesheet" type="text/css" href="css/jquery-ui.css">
+<link rel="stylesheet" type="text/css" href="css/layerslider.css">
+<link rel="stylesheet" type="text/css" href="css/color.css" id="color-change">
+<link rel="stylesheet" type="text/css" href="css/owl.carousel.min.css">
+<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="fonts/flaticon/flaticon.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+
+<!-- Title -->
+<title>Real Estate PHP</title>
+</head>
+<body>
+
+<!--	Page Loader -->
+<!-- <div class="page-loader position-fixed z-index-9999 w-100 bg-white vh-100">
+	<div class="d-flex justify-content-center y-middle position-relative">
+	  <div class="spinner-border" role="status">
+		<span class="sr-only">Loading...</span>
+	  </div>
+	</div>
+</div> -->
+<!-- FOR MORE PROJECTS visit: codeastro.com -->
+<div id="page-wrapper">
+    <div class="row"> 
+        <!--	Header start  -->
+		<?php include("include/header.php");?>
+        <!--	Header end  -->
+        
+        <!--	Banner   --->
+        <!-- <div class="banner-full-row page-banner" style="background-image:url('images/breadcromb.jpg');">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h2 class="page-name float-left text-white text-uppercase mt-1 mb-0"><b>About US</b></h2>
+                    </div>
+                    <div class="col-md-6">
+                        <nav aria-label="breadcrumb" class="float-left float-md-right">
+                            <ol class="breadcrumb bg-transparent m-0 p-0">
+                                <li class="breadcrumb-item text-white"><a href="#">Home</a></li>
+                                <li class="breadcrumb-item active">About Us</li>
+                            </ol>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div> -->
+         <!--	Banner   --->
+		 <!-- FOR MORE PROJECTS visit: codeastro.com -->
+        <!--	About Our Company -->
+        <div class="full-row">
+            <div class="container">
+                <H1>About Us </H1>
+            "Welcome to Real Estate , your trusted partner in the world of real estate. Whether you're in search of your dream home, looking to sell your property, or seeking a lucrative investment opportunity, we're here to guide you every step of the way.
+
+With a team of experienced real estate professionals and an extensive portfolio of properties, we're dedicated to helping you find the perfect match for your needs. Our mission is to make your real estate journey as smooth and rewarding as possible.
+
+Explore our listings, discover valuable resources, and get in touch with our experts today. Your real estate aspirations are our top priority, and we're committed to turning your dreams into reality.
+
+At there , we don't just sell properties; we build relationships and create futures. Welcome to a world of endless possibilities in real estate."
+
+Feel free to adapt and expand upon these words to create a compelling introduction that resonates with your website visitors and reflects your unique brand identity.
+
+				
+            </div>
+        </div>
+        <!--	About Our Company -->        
+        
+       <!--	Footer   start-->
+		<?php include("include/footer.php");?>
+		<!--	Footer   start-->
+        
+        <!-- Scroll to top --> 
+        <a href="#" class="bg-secondary text-white hover-text-secondary" id="scroll"><i class="fas fa-angle-up"></i></a> 
+        <!-- End Scroll To top --> 
+    </div><!-- FOR MORE PROJECTS visit: codeastro.com -->
+</div>
+<!-- Wrapper End --> 
+
+<!--	Js Link
+============================================================--> 
+<script src="js/jquery.min.js"></script> 
+<!--jQuery Layer Slider --> 
+<script src="js/greensock.js"></script> 
+<script src="js/layerslider.transitions.js"></script> 
+<script src="js/layerslider.kreaturamedia.jquery.js"></script> 
+<!--jQuery Layer Slider --> 
+<script src="js/popper.min.js"></script> 
+<script src="js/bootstrap.min.js"></script> 
+<script src="js/owl.carousel.min.js"></script> 
+<script src="js/tmpl.js"></script> 
+<script src="js/jquery.dependClass-0.1.js"></script> 
+<script src="js/draggable-0.1.js"></script> 
+<script src="js/jquery.slider.js"></script> 
+<script src="js/wow.js"></script> 
+<script src="js/jquery.cookie.js"></script> 
+<script src="js/custom.js"></script>
+</body>
+
+</html>
